22acdd3fffbba772290380c98a3790c31a285037
Khanh Tran <khanhtranngoccva@gmail.com>
2023-04-24 12:16:11 +0700

    fix: Delete user password and hash in API response

--- domains/users/controllers/currentUser.ts
+++ domains/users/controllers/currentUser.ts
@@ -1,5 +1,13 @@
 import express from "express";
 
 export default function getCurrentUser(req: express.Request, res: express.Response) {
-    res.json(req.user);
+    const user = req.user;
+    if (user) {
+        delete user.hash;
+        delete user.salt;
+        res.json(user);
+    }
+    else {
+        res.json(null);
+    }
 }
