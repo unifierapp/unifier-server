4aebe4faede3ab5699202fa77d6225d5578c7e40
Khanh Tran <khanhtranngoccva@gmail.com>
2023-04-28 22:52:38 +0700

    misc: Enable logging of emails

--- domains/emails/index.ts
+++ domains/emails/index.ts
@@ -4,5 +4,7 @@ import {MAIL_CONFIG} from "@/config";
 const transport = nodemailer.createTransport(MAIL_CONFIG);
 
 export async function sendNoReplyEmail(props: { to: string, html: string, subject: string }) {
-    await transport.sendMail({from: process.env.EMAIL_NOREPLY_SENDER, ...props});
+    const config = {from: process.env.EMAIL_NOREPLY_SENDER, ...props};
+    console.log(config, transport);
+    await transport.sendMail(config);
 }
