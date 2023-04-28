1a8237f648925db641fa27a78599403d4a5852fc
Khanh Tran <khanhtranngoccva@gmail.com>
2023-04-28 23:10:16 +0700

    fix: Regenerate possible missing email confirmation key

--- domains/auth/controllers/resendConfirmEmail.ts
+++ domains/auth/controllers/resendConfirmEmail.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import {sendConfirmEmail} from "@/domains/auth/services/sendConfirmEmail";
 import {UnauthorizedError} from "@/utils/errors";
+import crypto from "crypto";
 
 export default async function resendConfirmEmail(req: express.Request, res: express.Response) {
     if (!req.user) {
         throw new UnauthorizedError("You must log in before using this service.");
     }
+    if (!req.user.emailVerified && req.user.newEmail !== req.user.email) {
+        throw new UnauthorizedError("You have already been verified.");
+    }
     if (!req.user.emailConfirmationKey) {
-        throw new UnauthorizedError("You have already been verified.")
+        req.user.emailConfirmationKey = crypto.randomBytes(32).toString("hex");
     }
     await sendConfirmEmail(req, req.user.newEmail, req.user.emailConfirmationKey);
     res.send({});
