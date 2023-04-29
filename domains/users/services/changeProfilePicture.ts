9165100507fe44c30b50e581d07a34f130ec986f
Khanh Tran <khanhtranngoccva@gmail.com>
2023-04-29 21:44:05 +0700

    debug: Added picture path

--- domains/users/services/changeProfilePicture.ts
+++ domains/users/services/changeProfilePicture.ts
@@ -3,7 +3,7 @@ import * as fs from "fs";
 
 export default async function changeProfilePicture(user: Express.User, picture: Express.Multer.File) {
     const result = await uploader.upload(picture.path);
-    console.log(picture.path);
+    console.log(picture.path, result);
     await fs.promises.rm(picture.path);
     if (user.profilePictureCloudId) {
         await uploader.destroy(user.profilePictureCloudId);
