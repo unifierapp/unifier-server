a22e6c8b2a7a8652122e3ecb964d68f3e2b53815
Khanh Tran <khanhtranngoccva@gmail.com>
2023-04-29 21:56:51 +0700

    debug: Added picture upload debug logs

--- domains/users/controllers/changeProfilePicture.ts
+++ domains/users/controllers/changeProfilePicture.ts
@@ -4,6 +4,7 @@ import {BadArgumentError} from "@/utils/errors";
 import changeProfilePictureFunc from "@/domains/users/services/changeProfilePicture";
 
 export default async function changeProfilePicture(req: express.Request, res: express.Response) {
+    console.log("Parsing file.");
     await new Promise<void>((resolve, reject) => {
         try {
             imageUploader.single("profile_picture")(req, res, () => resolve);
@@ -14,6 +15,7 @@ export default async function changeProfilePicture(req: express.Request, res: ex
     if (!req.file) {
         throw new BadArgumentError("1 picture is required.");
     }
+    console.log("File received.");
     await changeProfilePictureFunc(req.user!, req.file);
     res.json({});
 }
--- domains/users/services/changeProfilePicture.ts
+++ domains/users/services/changeProfilePicture.ts
@@ -2,8 +2,9 @@ import uploader from "@/domains/upload/cloudinary";
 import * as fs from "fs";
 
 export default async function changeProfilePicture(user: Express.User, picture: Express.Multer.File) {
+    console.log("Uploading to Cloudinary.", picture.path);
     const result = await uploader.upload(picture.path);
-    console.log(picture.path, result);
+    console.log("Uploaded.", picture.path, result);
     await fs.promises.rm(picture.path);
     if (user.profilePictureCloudId) {
         await uploader.destroy(user.profilePictureCloudId);
