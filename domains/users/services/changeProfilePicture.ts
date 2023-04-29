1b66acd57f3d69b15435516bc9680492b5b8a3d5
Khanh Tran <khanhtranngoccva@gmail.com>
2023-04-29 19:33:39 +0700

    fix: Delete uploaded file from tmp directory for atomicity

--- domains/users/services/changeProfilePicture.ts
+++ domains/users/services/changeProfilePicture.ts
@@ -1,7 +1,9 @@
 import uploader from "@/domains/upload/cloudinary";
+import * as fs from "fs";
 
 export default async function changeProfilePicture(user: Express.User, picture: Express.Multer.File) {
     const result = await uploader.upload(picture.path);
+    await fs.promises.rm(picture.path);
     if (user.profilePictureCloudId) {
         await uploader.destroy(user.profilePictureCloudId);
     }
